5baae5a2dc3ce5dc0ca63f9b6204c3e72f22e41a
Lutfilah Ahmad <lutfilahahmad169@gmail.com>
2026-04-14 14:49:46 +0700

    buat subClass MadiunMotor dan MadiunMobil

--- AplikasiMadiunJek.java
+++ AplikasiMadiunJek.java
@@ -18,17 +18,76 @@ abstract class LayananTransportasi {
     public abstract void prosesTransaksi();
 }
 
+class MadiunMotor extends LayananTransportasi {
+    
+    public MadiunMotor(String namaDriver, double jarak, double saldoUser) {
+        super(namaDriver, jarak, saldoUser);
+    }
 
+    @Override
+    public double hitungTarif() {
+        return jarak * 2500; 
+    }
+
+    @Override
+    public void prosesTransaksi() {
+        System.out.println("=== Transaksi oleh Lutfilah Ahmad - 254311020 ===");
+        System.out.println("Saldo Awal: Rp " + saldoUser);
+        
+        double tarif = hitungTarif();
+        
+        if (saldoUser >= tarif) {
+            saldoUser -= tarif;
+            System.out.println("Nama Driver: " + namaDriver);
+            System.out.println("Jarak: " + jarak + " km");
+            System.out.println("Total Bayar: Rp " + tarif);
+            System.out.println("Sisa Saldo: Rp " + saldoUser);
+            System.out.println();
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+            System.out.println();
+        }
+    }
+}
+
+class MadiunMobil extends LayananTransportasi {
+    private double biayaAdmin;
+    
+    public MadiunMobil(String namaDriver, double jarak, double saldoUser) {
+        super(namaDriver, jarak, saldoUser);
+        int nim = 20;
+        this.biayaAdmin = 3000 + (nim * 100); 
+    }
+
+    @Override
+    public double hitungTarif() {
+        double tarifDasar = jarak * 5000; 
+        return tarifDasar + biayaAdmin;
+    }
+
+    @Override
+    public void prosesTransaksi() {
+        System.out.println("=== Transaksi oleh Lutfilah Ahmad - 254311020 ===");
+        System.out.println("Saldo Awal: Rp " + saldoUser);
+        
+        double tarif = hitungTarif();
+        
+        if (saldoUser >= tarif) {
+            saldoUser -= tarif;
+            System.out.println("Nama Driver: " + namaDriver);
+            System.out.println("Jarak: " + jarak + " km");
+            System.out.println("Total Bayar (Tarif + Biaya Admin Rp " + biayaAdmin + "): Rp " + tarif);
+            System.out.println("Sisa Saldo: Rp " + saldoUser);
+            System.out.println();
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+            System.out.println();
+        }
+    }
+}
 
 public class AplikasiMadiunJek {
     public static void main(String[] args) {
-        LayananTransportasi[] layanan = new LayananTransportasi[2];
         
-        layanan[0] = new MadiunMotor("Budi", 10, 50000);
-        layanan[1] = new MadiunMobil("Andi", 5, 100000);
-        
-        for (LayananTransportasi l : layanan) {
-            l.prosesTransaksi();
-        }
     }
 }
