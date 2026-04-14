c9ac5c649079abbcb0640cfa2468f5c9d89adb78
Lutfilah Ahmad <lutfilahahmad169@gmail.com>
2026-04-14 14:50:56 +0700

    Polymorphic Testing dengan looping

--- AplikasiMadiunJek.java
+++ AplikasiMadiunJek.java
@@ -88,6 +88,13 @@ class MadiunMobil extends LayananTransportasi {
 
 public class AplikasiMadiunJek {
     public static void main(String[] args) {
+        LayananTransportasi[] layanan = new LayananTransportasi[2];
         
+        layanan[0] = new MadiunMotor("Budi", 10, 50000);
+        layanan[1] = new MadiunMobil("Andi", 5, 100000);
+        
+        for (LayananTransportasi l : layanan) {
+            l.prosesTransaksi();
+        }
     }
 }
